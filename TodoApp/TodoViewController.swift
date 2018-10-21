a6d702d96c058dedd0cfe7d5a18860f333024f3e
Ahmad Hamed Rangeen <hamedrangeen@Ahmads-MacBook-Pro.local>
2018-10-20 17:41:33 -0700

    DataSource added

--- TodoApp/TodoViewController.swift
+++ TodoApp/TodoViewController.swift
@@ -2,4 +2,19 @@ import UIKit
 
 class TodoViewController : UITableViewController {
     
+    
+}
+
+
+// Datasource
+extension TodoViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Todo"
+        return cell
+    }
 }
